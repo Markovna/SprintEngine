22ae0fd7c36e4e67859cfa24f39d9e0d707a8c6c
abdushukurova <abdushukurova.km@gmail.com>
2020-08-27 16:36:10 +0300

    camera

---
+++ renderer/Camera.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Matrix.h"
+
+class Camera {
+public:
+    explicit Camera(const Matrix& projection) :
+        m_Position(Vec3::Zero),
+        m_Rotation(Quat::Identity),
+        m_ProjectionMatrix(projection)
+    {}
+
+    void SetRotation(const Quat& rotation) { m_Rotation = rotation; }
+    void SetPosition(const Vec3& position) { m_Position = position; }
+    void SetProjectionMatrix(const Matrix& mat) { m_ProjectionMatrix = mat; }
+
+    Matrix GetProjectionMatrix() const { return m_ProjectionMatrix; }
+    Matrix GetViewMatrix() const { return Matrix::Translation(m_Position) * Matrix::Rotation(m_Rotation); }
+
+private:
+    Matrix m_ProjectionMatrix;
+
+    Vec3 m_Position;
+    Quat m_Rotation;
+};
+
+
