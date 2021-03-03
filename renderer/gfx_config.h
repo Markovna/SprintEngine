9d57b92086fa2bcbce95d6893aea8e0e8bab0ef1
abdushukurova <abdushukurova.km@gmail.com>
2021-03-03 10:19:13 +0300

    .

--- renderer/gfx_config.h
+++ renderer/gfx_config.h
@@ -17,7 +17,6 @@ constexpr static const uint16_t kUniformsCapacity = 2048;
 constexpr static const uint16_t kShadersCapacity = 1024;
 constexpr static const uint16_t kTexturesCapacity = 1024;
 constexpr static const size_t kMaxDrawCallsCount = 2048;
-constexpr static const uint16_t kMaxCommandCountPerDrawCall = 2048;
 constexpr static const uint16_t kMaxUniformsPerDrawCall = 128;
 constexpr static const uint16_t kMaxFrameCommandsCount = 2048;
 
